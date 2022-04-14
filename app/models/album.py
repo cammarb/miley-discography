2f90de5670ee60f15d26de6310da7a274b223163
Camila Martinez <camilamarb98@gmail.com>
2022-04-14 17:38:44 -0500

    add Song and Album models

---
+++ app/models/album.py
@@ -0,0 +1,15 @@
+from app.extensions.database import db, CRUD_mixing
+
+
+class Album(db.Model, CRUD_mixing):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    slug = db.Column(db.String, nullable=False, unique=True)
+    release_date = db.Column(db.Date, nullable=False)
+    is_live = db.Column(db.Boolean, nullable=False)  # if True, album is live version
+    is_ep = db.Column(db.Boolean, nullable=False)  # if True, it's an EP (Extended Play)
+    number_of_songs = db.Column(db.Integer)
+    tracklist = db.relationship(
+        "Song", back_populates="album", cascade="all, delete-orphan"
+    )
+    total_lenght = db.Column(db.Time)
---
+++ app/models/song.py
@@ -0,0 +1,13 @@
+from sqlalchemy import ForeignKey
+from app.extensions.database import db, CRUD_mixing
+
+
+class Song(db.Model, CRUD_mixing):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    slug = db.Column(db.String, nullable=False, unique=True)
+    artist = db.Column(db.String, nullable=False)
+    featuring = db.Column(db.String)
+    lenght = db.Column(db.Time)
+    album_id = db.Column(db.Integer, ForeignKey("album.id"), nullable=False)
+    album = db.relationship("Album", back_populates="tracklist")
