5b8a254fc7a9e37f344bf553f824e15ab1300aa2
Camila Martinez <camilamarb98@gmail.com>
2022-04-14 18:36:46 -0500

    add registration of blueprints and db

--- app/app.py
+++ app/app.py
@@ -1,13 +1,23 @@
-import imp
 from flask import Flask
-from app.models import album
+from app import routes
+from app.extensions.database import db
+from . import routes
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("app.config")
+
+    register_extensions(app)
+    register_blueprints(app)
+
     return app
 
 
-# def register_blueprints(app: Flask):
-#     return
+def register_extensions(app: Flask):
+    db.init_app(app)
+
+
+def register_blueprints(app: Flask):
+    app.register_blueprint(routes.songs.blueprint)
+    app.register_blueprint(routes.albums.blueprint)
