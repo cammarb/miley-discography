f804d1a898ea81b220df3715acc0bdeda482632e
Camila Martinez <camilamarb98@gmail.com>
2022-04-14 19:12:42 -0500

    add album route

--- app/routes/albums.py
+++ app/routes/albums.py
@@ -1,5 +1,10 @@
 from flask import Flask, redirect, url_for, render_template
 from flask import Blueprint
-
+from app.models.album import Album
 
 blueprint = Blueprint("albums", __name__)
+
+
+@blueprint.get("/")
+def get_albums():
+    return render_template("list_albums.html")
