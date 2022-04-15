b6f68399bd0002b0b791dff87aa1b5b4a0efe314
Camila Martinez <camilamarb98@gmail.com>
2022-04-14 20:05:28 -0500

    testing routes

--- app/routes/albums.py
+++ app/routes/albums.py
@@ -7,4 +7,21 @@ blueprint = Blueprint("albums", __name__)
 
 @blueprint.get("/albums")
 def get_albums():
-    return render_template("albums/list_albums.html")
+    albums = Album.query.all()
+    return render_template("albums/list_albums.html", albums=albums)
+
+
+# @blueprint.get("/albums")
+# def get_album():
+#     albums = Album.query.all()
+#     return render_template("albums/list_albums.html", albums=albums)
+
+
+# @blueprint.get("/albums/<int:id>")
+# def get_album(id):
+#     album = Album.query.get(id)
+#     return render_template("albums/.html", album=album)
+
+
+# @blueprint.post('/albums')
+# def add_album():
