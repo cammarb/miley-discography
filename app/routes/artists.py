cefa14dcf296dc36a22decb901da58c219ec8b80
Camila Martinez <camilamarb98@gmail.com>
2022-04-19 15:45:54 -0500

    refactor artists routes code

--- app/routes/artists.py
+++ app/routes/artists.py
@@ -1,20 +1,37 @@
-from flask import Blueprint, jsonify, render_template, request, url_for
+from gzip import READ
+from flask import Blueprint, jsonify, redirect, render_template, request, url_for
 
 from app.models.artist import Artist
+from app.services.main import get_all, get_one
+from app.services.artists import create_artist
 
 blueprint = Blueprint("artists", __name__)
 
 
-@blueprint.get("/artist")
-def get_artist():
-    return render_template("artist/add_artist.html")
+@blueprint.get("/artists")
+def get_artists():
+    artists = get_all(Artist)
+    return render_template("artists/list_artists.html", artists=artists)
 
 
-@blueprint.post("/artist")
+# Delete artist from table
+@blueprint.post("/artists")
+def delete_artist():
+    artist = get_one(Artist, request.form.get("artist_id"))
+    artist.delete()
+    return redirect(url_for("artists.get_artists"))
+
+
+# Add artist
+@blueprint.get("/artists/add")
+def add_artist():
+    return render_template("artists/add_artist.html")
+
+
+@blueprint.post("/artists/add")
 def post_artist():
-    new_artist = Artist(name=request.form.get("name"))
-    new_artist.save()
-    return jsonify({"id": new_artist.id, "name": new_artist.name})
+    create_artist(request.form)
+    return redirect(url_for("artists.get_artists"))
 
 
 # @blueprint.get("/artist/<int:id>")
