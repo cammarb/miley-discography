c807092cadec6c6770c36ef57ab7a0e1b5e08f46
Camila Martinez <camilamarb98@gmail.com>
2022-04-18 19:45:07 -0500

    implement services | Refactoring

--- app/routes/albums.py
+++ app/routes/albums.py
@@ -1,51 +1,29 @@
 from flask import Flask, jsonify, redirect, request, url_for, render_template
 from flask import Blueprint
-from datetime import datetime
 from app.models.album import Album
 from app.models.artist import Artist
-
-# from app.models.artist import Artist
+from app.services.main import get_all, get_one
+from app.services.albums import create_album
 
 blueprint = Blueprint("albums", __name__)
 
 
 @blueprint.get("/albums")
 def get_albums():
-    albums = Album.query.all()
+    albums = get_all(Album)
     return render_template("albums/list_albums.html", albums=albums)
 
 
 @blueprint.get("/albums/add_album")
 def get_album():
-    artists = Artist.query.all()
+    artists = get_all(Artist)
     return render_template("albums/add_album.html", artists=artists)
 
 
 @blueprint.post("/albums/add_album")
 def post_album():
-    new_album = Album(
-        title=request.form.get("title"),
-        slug=request.form.get("slug"),
-        release_date=datetime.strptime(request.form.get("release_date"), "%Y-%m-%d"),
-        is_live=request.form.get("is_live"),
-        is_ep=request.form.get("is_ep"),
-        number_of_songs=0,
-        total_length=request.form.get("total_length"),
-        artist_id=request.form.get("artist_id"),
-    )
-    new_album.save()
-    return jsonify(
-        {
-            "id": new_album.id,
-            "title": new_album.title,
-            "slug": new_album.slug,
-            "release_date": new_album.release_date,
-            "is_live": new_album.is_live,
-            "is_ep": new_album.is_ep,
-            "total_length": new_album.total_length,
-            "artist_id": new_album.artist_id,
-        }
-    )
+    create_album(request.form)
+    return {"Album successfully created."}
 
 
 # @blueprint.get("/albums/<int:id>")
---
+++ app/services/albums.py
@@ -0,0 +1,17 @@
+from app.models.album import Album
+from app.models.artist import Artist
+from datetime import datetime
+
+
+def create_album(req_form):
+    new_album = Album(
+        title=req_form.get("title"),
+        slug=req_form.get("slug"),
+        release_date=datetime.strptime(req_form.get("release_date"), "%Y-%m-%d"),
+        is_live=req_form.get("is_live"),
+        is_ep=req_form.get("is_ep"),
+        number_of_songs=0,
+        total_length=req_form.get("total_length"),
+        artist_id=req_form.get("artist_id"),
+    )
+    new_album.save()
