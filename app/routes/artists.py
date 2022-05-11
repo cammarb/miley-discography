b83237c442b8b8f625f804ca1c1b8533788ce75c
Camila Martinez <camilamarb98@gmail.com>
2022-05-10 20:01:15 -0500

    update artist routes

--- app/routes/artists.py
+++ app/routes/artists.py
@@ -3,7 +3,7 @@ from flask import Blueprint, jsonify, redirect, render_template, request, url_fo
 
 from app.models.artist import Artist
 from app.services.main import get_all, get_one
-from app.services.artists import create_artist, update_artist, delete
+from app.services.artists import create_artist, edit_artist, delete
 
 blueprint = Blueprint("artists", __name__)
 
@@ -12,26 +12,6 @@ blueprint = Blueprint("artists", __name__)
 def get_artists():
     artists = get_all(Artist)
     return render_template("artists/list_artists.html", artists=artists)
-
-
-# Edit artist
-@blueprint.get("/artists/<int:id>")
-def get_artist(id):
-    artist = get_one(Artist, id)
-    return render_template("artists/artist.html", artist=artist)
-
-
-@blueprint.post("/artists/<int:id>")
-def edit_artist(id):
-    update_artist(request.form, id)
-    return redirect(url_for("artists.get_artists"))
-
-
-# Delete artist
-@blueprint.post("/artists/<int:id>")
-def delete_artist(id):
-    artist.delete(id)
-    return redirect(url_for("artists.get_artists"))
 
 
 # Add artist
@@ -46,6 +26,21 @@ def post_artist():
     return redirect(url_for("artists.get_artists"))
 
 
-# @blueprint.get("/artist/<int:id>")
-# def get_single_artist(id):
-#     return render_template("artist/add_artist.html")
+# Edit artist
+@blueprint.get("/artists/<int:id>")
+def get_artist(id):
+    artist = get_one(Artist, id)
+    return render_template("artists/artist.html", artist=artist)
+
+
+@blueprint.post("/artists/<int:id>/update")
+def update_artist(id):
+    edit_artist(request.form, id)
+    return redirect(url_for("artists.get_artists"))
+
+
+# Delete artist
+@blueprint.post("/artists/<int:id>/delete")
+def delete_artist(id):
+    delete(id)
+    return redirect(url_for("artists.get_artists"))
