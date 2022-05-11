cca1e675a164a19a2c36ab2d0c82d31d989a7204
Camila Martinez <camilamarb98@gmail.com>
2022-05-10 20:01:25 -0500

    update artist relationship with albums

--- app/models/artist.py
+++ app/models/artist.py
@@ -4,4 +4,6 @@ from app.extensions.database import db, CRUD_mixing
 class Artist(db.Model, CRUD_mixing):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    albums = db.relationship("Album", backref="artist", lazy=True)
+    albums = db.relationship(
+        "Album", backref="artist", lazy=True, cascade="all, delete-orphan"
+    )
