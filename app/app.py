be66ebe7489ba30b6611e8b02d7e24f67d97966b
Camila Martinez <camilamarb98@gmail.com>
2022-05-17 18:29:55 -0500

    update app file

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask
 from app import routes
 from app.extensions.database import db, migrate
-from . import routes
+from app.extensions.auth import login_manager
 
 
 def create_app():
@@ -17,6 +18,7 @@ def create_app():
 def register_extensions(app: Flask):
     db.init_app(app)
     migrate.init_app(app, db, render_as_batch=True)
+    login_manager.init_app(app)
 
 
 def register_blueprints(app: Flask):
@@ -24,3 +26,4 @@ def register_blueprints(app: Flask):
     app.register_blueprint(routes.albums.blueprint)
     app.register_blueprint(routes.artists.blueprint)
     app.register_blueprint(routes.static_pages.blueprint)
+    app.register_blueprint(routes.auth.blueprint)
