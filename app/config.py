8c3049e00e4ec8cc45fd09a646b275c369eec813
Camila Martinez <camilamarb98@gmail.com>
2022-06-02 17:27:25 -0500

    update config

--- app/config.py
+++ app/config.py
@@ -3,5 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL").replace(
+    "postgres://", "postgresql://", 1
+)
 SECRET_KEY = environ.get("SECRET_KEY")
